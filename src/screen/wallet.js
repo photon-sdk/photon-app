2292129f7633ea540f2af26d7974750052624f07
Tankred Hase <mail@tankredhase.de>
2020-05-21 12:53:23 +0800

    Implement balance display on home screen

--- src/screen/wallet.js
+++ src/screen/wallet.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {observer} from 'mobx-react';
 
+import {LargeSpinner} from '../component/spinner';
+
 import store from '../store';
 
 const styles = StyleSheet.create({
@@ -11,21 +13,37 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 25,
   },
-  h1: {
-    fontSize: 30,
-  },
-  xpub: {
-    marginTop: 50,
-    fontSize: 20,
-  },
 });
 
 const WalletScreen = () => (
   <View style={styles.wrapper}>
-    <Text style={styles.h1}>
-      Wallet {store.backupExists !== false ? 'Restored' : 'Backed Up'}!
+    {store.balance === null ? <LargeSpinner /> : <Balance />}
+  </View>
+);
+
+const balanceStyles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 30,
+  },
+  numeral: {
+    marginTop: 10,
+    fontSize: 50,
+  },
+});
+
+const Balance = () => (
+  <View style={balanceStyles.wrapper}>
+    <Text style={balanceStyles.label}>Total sats</Text>
+    <Text
+      style={balanceStyles.numeral}
+      adjustsFontSizeToFit={true}
+      numberOfLines={1}>
+      {store.balance}
     </Text>
-    <Text style={styles.xpub}>Public key: {store.xpub}</Text>
   </View>
 );
 
