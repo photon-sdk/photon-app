49e0516113a2af002b9bd0b7dac14c1de3a941b5
Tankred Hase <mail@tankredhase.de>
2020-05-25 14:23:39 +0800

    Add icons to bottom tab navigation

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
 
 import './action';
 import {setTopLevelNavigator} from './action/nav';
@@ -23,8 +24,25 @@ const BackupStackScreen = () => (
 );
 
 const MainStackScreen = () => (
-  <MainStack.Navigator>
+  <MainStack.Navigator
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        if (route.name === 'Wallet') {
+          iconName = 'list';
+        } else if (route.name === 'Send') {
+          iconName = 'send';
+        } else if (route.name === 'Receive') {
+          iconName = 'download';
+        } else if (route.name === 'Settings') {
+          iconName = 'settings';
+        }
+        return <Feather name={iconName} size={size} color={color} />;
+      },
+    })}>
     <MainStack.Screen name="Wallet" component={WalletScreen} />
+    <MainStack.Screen name="Send" component={WalletScreen} />
+    <MainStack.Screen name="Receive" component={WalletScreen} />
   </MainStack.Navigator>
 );
 
