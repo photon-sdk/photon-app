bca3bd36e3c7e9776cc27fcc757209974ec05300
Tankred Hase <mail@tankredhase.de>
2020-08-24 14:47:44 +0200

    Fix receive screen address find size to fit in one line

--- src/screen/receive.js
+++ src/screen/receive.js
@@ -6,6 +6,7 @@ import {Text} from '../component/text';
 import {QRCode} from '../component/qrcode';
 import {PillButton} from '../component/button';
 import {LargeSpinner} from '../component/spinner';
+import {font} from '../component/style';
 
 import store from '../store';
 import * as wallet from '../action/wallet';
@@ -23,6 +24,8 @@ const styles = StyleSheet.create({
   },
   addressText: {
     marginTop: 20,
+    fontSize: font.sizeSub,
+    lineHeight: font.lineHeightSub,
   },
   btnWrapper: {
     alignSelf: 'stretch',
@@ -45,7 +48,12 @@ const ReceiveScreen = ({navigation}) => {
 const Address = () => (
   <View style={styles.addressWrapper}>
     <QRCode size={260}>{store.nextAddress}</QRCode>
-    <Text style={styles.addressText}>{store.nextAddress}</Text>
+    <Text
+      style={styles.addressText}
+      adjustsFontSizeToFit={true}
+      numberOfLines={1}>
+      {store.nextAddress}
+    </Text>
     <View style={styles.btnWrapper}>
       <PillButton onPress={() => wallet.copyAddress()}>Copy Address</PillButton>
     </View>
