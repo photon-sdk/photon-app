799e0851a4efe30ce3168e3c31878a2972b8e7e4
Tankred Hase <mail@tankredhase.de>
2020-09-21 17:08:24 +0200

    Add iCloud warning in recovery email screen

--- src/screen/email-set.js
+++ src/screen/email-set.js
@@ -2,11 +2,12 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {observer} from 'mobx-react';
 
-import {H1Text} from '../component/text';
+import {H1Text, Text} from '../component/text';
 import {TextInput} from '../component/input';
 import {PillButton} from '../component/button';
 import {MainContent, Spacer} from '../component/layout';
 import {Background} from '../component/background';
+import {font} from '../component/style';
 
 import store from '../store';
 import * as userId from '../action/user-id';
@@ -14,6 +15,13 @@ import * as userId from '../action/user-id';
 const styles = StyleSheet.create({
   input: {
     marginTop: 20,
+  },
+  hint: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    color: 'red',
+    fontSize: font.sizeSub,
+    lineHeight: font.lineHeightSub,
   },
   btnWrapper: {
     marginTop: 30,
@@ -24,15 +32,18 @@ const styles = StyleSheet.create({
 const EmailSetScreen = () => (
   <Background>
     <MainContent>
-      <H1Text>Enter your email address</H1Text>
+      <H1Text>Set recovery email</H1Text>
       <TextInput
-        placeholder="email"
+        placeholder="email address"
         keyboardType="email-address"
         autoFocus
         style={styles.input}
         value={store.userId.email}
         onChangeText={email => userId.setEmail(email)}
       />
+      <Text style={styles.hint}>
+        Use a different email than your iCloud account.
+      </Text>
       <Spacer />
       <View style={styles.btnWrapper}>
         <PillButton onPress={() => userId.initPinSet()}>Next</PillButton>
