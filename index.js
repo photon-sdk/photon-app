0a4bf4f9479dea9b005368976fda84184d198dcb
Tankred Hase <mail@tankredhase.de>
2020-10-27 18:59:59 +0100

    Inject Intl polyfill

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
  */
 
 import 'node-libs-react-native/globals';
+
+// Polyfill ECMAScript Internationalization API on Android
+// See: https://github.com/facebook/react-native/issues/19410
+import 'intl';
+import 'intl/locale-data/jsonp/en-US';
+
 import 'react-native-gesture-handler';
 import 'mobx-react-lite/batchingForReactNative';
 
