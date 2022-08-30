7f0cd91b2652835ae97a64c680bc45092d7ba577
Tankred Hase <mail@tankredhase.de>
2022-08-30 20:58:54 +0100

    Replace demo App.js

--- rn-0.69/index.js
+++ rn-0.69/index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
+import 'node-libs-react-native/globals';
+
+// Polyfill ECMAScript Internationalization API on Android
+// See: https://github.com/facebook/react-native/issues/19410
+import 'intl';
+import 'intl/locale-data/jsonp/en-US';
+
+import 'react-native-gesture-handler';
+import 'mobx-react-lite/batchingForReactNative';
+
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
